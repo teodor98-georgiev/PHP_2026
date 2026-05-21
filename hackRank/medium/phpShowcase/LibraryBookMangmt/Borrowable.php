df24e476b20679bec50459065ebe58b3b69b640e
Lenovo <teodor98.georgiev@gmail.com>
2026-05-21 14:00:45 +0300

    initial commit

--- hackRank/medium/phpShowcase/LibraryBookMangmt/Borrowable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface Borrowable
-{
-    public function checkout() ;   // mark item as unavailable
-    public function returnItem();     // mark item as available again
-    public function isAvailable(); // return the current availability
-}
